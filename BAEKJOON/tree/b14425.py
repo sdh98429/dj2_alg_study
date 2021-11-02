e07361507385a46b42319b9ae0ae3261076735e7
신동호 <sdh98429@naver.com>
2021-11-02 23:41:23 +0900

    1102 tree 문자열 집합 짱쉬움

---
+++ BAEKJOON/tree/b14425.py
@@ -0,0 +1,13 @@
+# tree 문자열 집합
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+S = [input() for _ in range(N)]
+
+cnt = 0
+for _ in range(M):
+    word = input()
+    if word in S:
+        cnt += 1
+
+print(cnt)
