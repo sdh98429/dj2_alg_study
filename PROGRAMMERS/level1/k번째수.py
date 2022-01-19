92ef12ac8c636916505ceeeee9e1f763462ebcbd
sdh98429 <87460973+sdh98429@users.noreply.github.com>
2022-01-19 21:49:35 +0900

    0119 level1 k번째수

---
+++ PROGRAMMERS/level1/k번째수.py
@@ -0,0 +1,13 @@
+# k번째 수
+# 42748
+
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i, j, k = command
+        answer.append(sorted(array[i - 1:j])[k - 1])
+
+    return answer
+
+# array	commands	return
+# [1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
